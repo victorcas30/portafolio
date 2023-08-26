69228add68f47f2a07aebbce24c2cf8742260f54
victorcas30 <vcastillo.mancia@gmail.com>
2023-08-25 23:23:10 -0600

    modo oscuro en etiquetas a i

--- src/components/portfolio/IconLink.jsx
+++ src/components/portfolio/IconLink.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './IconLink.css'; // Asegúrate de importar los estilos locales
+
 
 function IconLink(props) {
    const {link, title, icon} = props;
    return (
-      <a href={link} target={"_blank"} rel="noopener noreferrer">
+      <a href={link} target={"_blank"} rel="noopener noreferrer" className={props.theme === 'dark' ? 'dark' : ''}>
          <i className={icon}/> {title}
       </a>
    );
